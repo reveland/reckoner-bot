f8364fee186425fdaf2fe77d39258175d9292ebf
reveland <reveland92@gmail.com>
2017-11-04 23:39:21 +0100

    fix endpoint

--- app.py
+++ app.py
@@ -66,11 +66,11 @@ def webhook():
 
     return "ok", 200
 
-@app.route("/habitations/<int:habitant_id>/residents/<int:resident_id>/dept")
-def get_dept(habitant_id, resident_id):
-    dept = "# %d #" % RENT_RECKONER.get_debt(
-        habitant_id, DATA_PROVIDER.get_resident_by_id(habitant_id, resident_id))
-    resp = flask.Response(dept)
+@app.route("/habitations/<int:habitant_id>/residents")
+def get_residents(habitant_id):
+    residents = json.dumps(RENT_RECKONER.get_residents_to_ui(
+        habitant_id), default=lambda o: o.__dict__)
+    resp = flask.Response(residents)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
