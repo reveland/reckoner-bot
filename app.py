253ca3645746eee73e4d4041f5cc11d7952ff891
reveland <reveland92@gmail.com>
2017-11-04 12:48:43 +0100

    fix

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def webhook():
 
     return "ok", 200
 
-@APP.route("/habitations/<int:habitant_id>/residents/<int:resident_id>/dept")
+@app.route("/habitations/<int:habitant_id>/residents/<int:resident_id>/dept")
 def get_dept(habitant_id, resident_id):
     dept = "# %d #" % RENT_RECKONER.get_debt(
         habitant_id, DATA_PROVIDER.get_resident_by_id(habitant_id, resident_id))
@@ -69,7 +69,7 @@ def get_dept(habitant_id, resident_id):
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
-@APP.route("/habitations/<int:habitant_id>/bills")
+@app.route("/habitations/<int:habitant_id>/bills")
 def get_bills(habitant_id):
     bills = json.dumps(RENT_RECKONER.get_bills_to_ui(
         habitant_id), default=lambda o: o.__dict__)
