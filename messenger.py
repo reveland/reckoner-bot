4a214cfa6f7512b6c49dbef02bd514a8c7423a5e
reveland <reveland92@gmail.com>
2018-04-08 22:50:33 +0200

    15: add payment/get payments options

--- messenger.py
+++ messenger.py
@@ -6,6 +6,7 @@ import json
 base = 'https://agile-stream-12274.herokuapp.com/'
 bills_url = base + 'habitations/1/bills'
 resident_url = base + 'habitations/1/residents'
+payments_url = base + 'habitations/1/payments'
 update_debts_url = base + 'habitations/1/update_depts'
 
 class Messenger(object):
@@ -15,8 +16,10 @@ class Messenger(object):
                 return ('I see.. Well, then see into the green bowl under the black hat..\n'
             +'add_bill:\n'
             +'add_resident\n'
+            +'add_payments\n'
             +'get_bills\n'
             +'get_residents\n'
+            +'add_payments\n'
             +'update_debts')
             ms = ms.split('\n')
             if len(ms) == 1:
@@ -33,10 +36,14 @@ class Messenger(object):
                 return str(self.add_bill(0, m[1], m[2], m[3], float(m[4]), m[5]))
             elif m[0] == 'add_resident':
                 return str(self.add_resident(0, m[1], m[2], m[3]))
+            elif m[0] == 'add_payments':
+                return str(self.add_payment(0, m[1], m[2], m[3], m[4]))
             elif m[0] == 'get_bills':
                 return str(self.get_bills(0))
             elif m[0] == 'get_residents':
                 return str(self.get_residents(0))
+            elif m[0] == 'get_payments':
+                return str(self.get_payments(0))
             elif m[0] == 'update_debts':
                 return str(self.update_debts(0))
             else:
@@ -50,6 +57,10 @@ class Messenger(object):
         residents = requests.get(resident_url).json()
         return '\n'.join(map(lambda r: r['name'] + '\'s debt is ' + str(r['dept']), residents))
 
+    def get_payments(self, resident_id):
+        payments = requests.get(payments_url).json()
+        return '\n'.join(map(lambda r: r['receiver'] + '\'s paid ' + str(r['payer']) + ' ' + str(r['amount']), payments))
+    
     def get_bills(self, resident_id):
         bills = requests.get(bills_url).json()
         return '\n'.join(map(lambda r: r['type'] + ' ' + str(r['start']) + ' ' + str(r['end']) + ' ' + str(r['amount']), bills))
@@ -61,6 +72,15 @@ class Messenger(object):
             "name": name
         })
         return requests.post(resident_url, headers=self.headers(), data=resident).text
+
+    def add_payment(self, resident_id, amount, payer, receiver, date):
+        payment = json.dumps({
+            "amount": str(amount),
+            "payer": payer,
+            "receiver": receiver,
+            "date": str(date)
+        })
+        return requests.post(payments_url, headers=self.headers(), data=payment).text
 
     def add_bill(self, resident_id, start, end, type, amount, paid_by):
         bill = json.dumps({
