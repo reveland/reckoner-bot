0a26b75f96d018da34c687b971397a68f0688db5
reveland <reveland92@gmail.com>
2018-05-09 19:30:35 +0200

    28: fix viber bot

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ import requests
 from flask import Flask, request, Response
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages import VideoMessage
+from viberbot.api.messages.text_message import TextMessage
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
 
 from messenger import Messenger
 
