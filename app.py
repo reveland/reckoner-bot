11e7a778b1b8093bea2f72f2046c305ffd3bbe90
reveland <reveland92@gmail.com>
2018-05-09 19:26:46 +0200

    28: create viber echo bot

--- app.py
+++ app.py
@@ -6,13 +6,47 @@ from threading import Timer
 import urllib2
 import flask
 import requests
-from flask import Flask, request
+from flask import Flask, request, Response
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
 
 from messenger import Messenger
 
 messenger = Messenger()
 
 app = Flask(__name__)
+
+bot_configuration = BotConfiguration(
+    name='Reckoner',
+    avatar='http://viber.com/avatar.jpg',
+    auth_token='47ae7c0b0d27d1ef-2fe58f2b05086cfb-8a87db5e8dd49f97'
+)
+viber = Api(bot_configuration)
+
+@app.route('/viber_bot', methods=['POST'])
+def incoming():
+    log("received request. post data: {0}".format(request.get_data()))
+    # every viber message is signed, you can verify the signature using this method
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status=403)
+
+    # this library supplies a simple way to receive a request object
+    viber_request = viber.parse_request(request.get_data())
+
+    if isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message
+        # lets echo back
+        viber.send_messages(viber_request.sender.id, [
+            message
+        ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text="thanks for subscribing!")
+        ])
+    elif isinstance(viber_request, ViberFailedRequest):
+        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+
+    return Response(status=200)
 
 @app.route('/', methods=['GET'])
 def verify():
